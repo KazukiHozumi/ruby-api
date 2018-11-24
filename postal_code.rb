9a8ff8d915e6cbdfa9dbf58fb83b630664f610d6
KazukiHozumi <hozumikazuki.8140@gmail.com>
2018-11-25 01:33:10 +0900

    ひとまず郵便番号のAPIを叩くことはできた

--- postal_code.rb
+++ postal_code.rb
@@ -1,3 +1,18 @@
 require 'net/http'
 require 'uri'
 require 'json'
+
+puts "↓↓　zipcodeを入力してください　↓↓"
+zipcode = gets.chomp
+
+params = URI.encode_www_form(zipcode: zipcode)
+uri = URI.parse("http://zipcloud.ibsnet.co.jp/api/search?#{params}")
+response = Net::HTTP.start(uri.host, uri.port) do |http|
+  http.open_timeout = 5
+  http.read_timeout = 10
+  http.get(uri.request_uri)
+end
+
+result = JSON.parse(response.body)
+
+puts result
